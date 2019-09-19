0d658b2d4d34c62d12aa63a71d2fec8749e86516
yamauz <yamauzy@gmail.com>
2019-09-19 15:19:12 +0900

    Add chocolatey.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,12 +1,8 @@
-function Run-AsAdmin {
-    sudo choco install $args
-}
-
 
 Import-Module 'posh-git'
 Import-Module 'oh-my-posh'
 Set-Theme Paradox
-Set-Alias cinst Run-AsAdmin
 
 . C:\Users\Yarman\Documents\WindowsPowerShell\Alias\git.ps1
 . C:\Users\Yarman\Documents\WindowsPowerShell\Alias\yarn.ps1
+. C:\Users\Yarman\Documents\WindowsPowerShell\Alias\chocolatey.ps1
